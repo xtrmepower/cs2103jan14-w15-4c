0b2d1811f0ccc89b042c77fa191eeb8aa4729297
rey <rey@localhost>
2014-02-15 15:27:33 +0800

    Added textbox, Removed extra setup code

--- Code/MariaApp/MariaUI.h
+++ Code/MariaApp/MariaUI.h
@@ -6,54 +6,35 @@
 #include <QtWidgets/QButtonGroup>
 #include <QtWidgets/QHeaderView>
 #include <QtWidgets/QMainWindow>
-#include <QtWidgets/QMenuBar>
-#include <QtWidgets/QStatusBar>
-#include <QtWidgets/QToolBar>
-#include <QtWidgets/QWidget>
+#include <QtWidgets/QlineEdit>
 
 class MariaUI :	public QMainWindow
 {
 public:
 	MariaUI(void);
 	~MariaUI(void);
-	QMenuBar *menuBar;
-    QToolBar *mainToolBar;
-    QWidget *centralWidget;
-    QStatusBar *statusBar;
+	QLineEdit *inputBox;
 
     void setupUi()
     {
         if (this->objectName().isEmpty())
             this->setObjectName(QStringLiteral("MariaUI"));
 
-        this->resize(480, 160);
+        this->resize(480, 120);
 
-        menuBar = new QMenuBar(this);
-        this->menuBar->setObjectName(QStringLiteral("menuBar"));
-        this->setMenuBar(menuBar);
-
-        mainToolBar = new QToolBar(this);
-        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
-        this->addToolBar(mainToolBar);
-
-        centralWidget = new QWidget(this);
-        centralWidget->setObjectName(QStringLiteral("centralWidget"));
-        this->setCentralWidget(centralWidget);
-
-        statusBar = new QStatusBar(this);
-        statusBar->setObjectName(QStringLiteral("statusBar"));
-        this->setStatusBar(statusBar);
+		inputBox = new QLineEdit(this);
+		inputBox->setGeometry(QRect(30, 30, 400, 30));
 
         retranslateUi();
 
-        QMetaObject::connectSlotsByName(this);
-    } // setupUi
+        //QMetaObject::connectSlotsByName(this);
+    } 
 
 private:
     void retranslateUi()
     {
         this->setWindowTitle(QApplication::translate("MariaUI", "M.A.R.I.A", 0));
-    } // retranslateUi
+    } 
 
 };
 
