7eeb04c0350154bf98e4f6d83e147b74c9f09544
melvyn <melvyn@localhost>
2014-02-24 16:54:18 +0800

    Removed redundant #include directories from Project Settings

--- Code/MariaApp/MariaLogic.cpp
+++ Code/MariaApp/MariaLogic.cpp
@@ -1,20 +1,15 @@
 #include "MariaLogic.h"
 
-MariaLogic::MariaLogic(int argc, char *argv[]) : QApplication(argc, argv)
-{
+MariaLogic::MariaLogic(int argc, char *argv[]) : QApplication(argc, argv) {
 	mariaUI = new MariaUI();
-	
 }
 
-
-MariaLogic::~MariaLogic(void)
-{
+MariaLogic::~MariaLogic(void) {
 	delete mariaUI;
 }
 
-
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 	MariaLogic mariaLogic(argc, argv);
+
 	return QApplication::exec();
 }
