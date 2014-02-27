cc8c759306798c731d4df0567429808928a6481f
JayChua <JayChua@localhost>
2014-02-27 17:43:50 +0800

    Added MariaUIRolling Derived classes. -MariaUICalendar to handle animations in the calendar screen. -MariaUISettings to handle animations in the settings screen.

---
+++ Code/MariaApp/MariaUICalendar.cpp
@@ -0,0 +1,10 @@
+#include "MariaUICalendar.h"
+
+
+MariaUICalendar::MariaUICalendar(QMainWindow *parent) : MariaUIRolling(parent) {
+
+}
+
+
+MariaUICalendar::~MariaUICalendar() {
+}
---
+++ Code/MariaApp/MariaUISetting.cpp
@@ -0,0 +1,10 @@
+#include "MariaUICalendar.h"
+
+
+MariaUISetting::MariaUISetting(QMainWindow *parent) : MariaUIRolling(parent) {
+
+}
+
+
+MariaUISetting::~MariaUISetting() {
+}
