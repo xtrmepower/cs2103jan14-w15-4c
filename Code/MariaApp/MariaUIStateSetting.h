9c4d7a996901a434f725e51eddba75055ab0b950
JayChua <JayChua@localhost>
2014-03-09 14:32:06 +0800

    Added Edit, Setting and Show state. Removed old UISetting state.

--- Code/MariaApp/MariaUIStateSetting.h
+++ Code/MariaApp/MariaUIStateSetting.h
@@ -3,14 +3,11 @@
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/qlabel.h>
 #include "MariaStateObject.h"
-#include "MariaUIClock.h"
 
-class MariaUIStateHome : public MariaStateObject {
+class MariaUIStateSetting : public MariaStateObject {
 	
 	QMainWindow *_qmainWindow;
 
-	MariaUIClock *_clock;
-	
 	void initBeginState();
 	void initActiveState();
 	void initEndState();
@@ -18,7 +15,7 @@ class MariaUIStateHome : public MariaStateObject {
 	bool timerActiveState();
 	bool timerEndState();
 public:
-	MariaUIStateHome(QMainWindow* qmainWindow);
-	~MariaUIStateHome();
+	MariaUIStateSetting(QMainWindow* qmainWindow);
+	~MariaUIStateSetting();
 };
 
