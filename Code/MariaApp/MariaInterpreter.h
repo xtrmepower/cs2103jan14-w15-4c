542b971e6bd319c69b0cf17a0b7c47717dfe1af7
melvyn <melvyn@localhost>
2014-03-10 22:46:44 +0800

    Input is now checked for validity.

--- Code/MariaApp/MariaInterpreter.h
+++ Code/MariaApp/MariaInterpreter.h
@@ -15,6 +15,7 @@ public:
 		AddFloatingTask,
 		AddDeadlineTask,
 		AddTimedTask,
+		EditTask,
 		ShowAllTask,
 		DeleteTask,
 		Exit,
@@ -24,35 +25,53 @@ public:
 	MariaInterpreter(map<string, CommandType>* inputCommandList = NULL);
 	~MariaInterpreter(void);
 
-	// Returns a CommandType depending on the first word in the
-	// inputString passed in. Upon reading a valid Command keyword,
-	// that keyword is removed from the inputString itself.
-	//
-	// If no valid keyword is detected, a Invalid CommandType is
-	// returned.
+	/// Gets a CommandType flag depending on the user's input.
+	/// The command keyword would also be removed from the inputString.
+	///
+	/// @param	inputString	User's input.
+	///
+	/// @return	The command's flag will be returned on valid input. Invalid flag otherwise.
 	CommandType getCommandType(string &inputString);
 
-	// Same as above, but hoping it works better with regex.
-	CommandType getCommandTypeRegex(string &inputString);
-
+	/// Gets the task's title from the user's input.
+	/// The title would also be removed from the inputString.
+	///
+	/// @param	inputString	User's input.
+	///
+	/// @return	The task's title would be returned on valid input. Blank otherwise.
 	string getTitle(string &inputString);
 
 	MariaTime* getStartTime(string &inputString);
 	MariaTime* getEndTime(string &inputString);
 
+#ifdef _DEBUG
+public:
+#else
 private:
+#endif
 	map<string, CommandType> *userDefinedCommands;
 
-	// Returns a new string that has the oldText replaced with
-	// the newText from the inputString itself. Also, firstInstanceOnly
-	// determines if only the first instance of the oldText
-	// is replaced.
-	//
-	// By default, firstInstanceOnly is set to true.
+	/// Checks the validity of the user's input depending on
+	/// the presence of certain keywords, number of words and so on.
+	///
+	/// @param	User's input.
+	///
+	/// @return	True if valid. False otherwise.
+	bool checkInputValidity(string inputString);
+
+	/// Replaces oldText in the inputString with newText.
+	///
+	/// @param inputString			String to be modified.
+	/// @param oldText				Text to be replaced.
+	/// @param newText				Text to replace oldText
+	/// @param firstInstanceOnly	Whether only the first instance of oldText will be replaced. Default is true.
+	///
+	/// @return						String with oldText replaced with newText.
 	string replaceText(string inputString, string oldText, string newText, bool firstInstanceOnly = true);
 
-	//vector<string> tokenizeString(string inputString);
 	vector<string> tokenizeString(string inputString, char delimiter = ' ');
+
+	string lowercaseString(string text);
 
 	// To trim whitespace.
 	inline string trimWhiteSpaceLeft(string text);
