e5d96900be439b7af305be0019c8c9075584dafc
melvyn <melvyn@localhost>
2014-03-12 13:21:36 +0800

    Interpreter overhaul

--- Code/MariaApp/MariaLogic.cpp
+++ Code/MariaApp/MariaLogic.cpp
@@ -37,29 +37,29 @@ MariaLogic::~MariaLogic(void) {
 }
 
 bool MariaLogic::processCommand(std::string inputText) {
-	MariaInterpreter::CommandType commandType = mariaInterpreter->getCommandType(inputText);
+	//MariaInterpreter::CommandType commandType = mariaInterpreter->getCommandType(inputText);
+	MariaInputObject* input = mariaInterpreter->parseInput(inputText);
 	mariaUI->getCommandBar()->getTextbox()->setUserInput("");
 
-	if(commandType == MariaInterpreter::CommandType::Invalid) {
+	if(input->getCommandType() == MariaInputObject::CommandInvalid) {
 		mariaUI->getCommandBar()->getTextbox()->setQuestionText("I don't understand. Please try again.");
 		mariaUI->getCommandBar()->getStatus()->setStatus(MariaUIStatus::UNKNOWN);
 		return false;
 	} else {
 		mariaUI->getCommandBar()->getStatus()->setStatus(MariaUIStatus::OK);
 
-		if(commandType == MariaInterpreter::CommandType::Exit){
+		if(input->getCommandType() == MariaInputObject::CommandExit){
 			MariaUIStateLoading *temp = new MariaUIStateLoading((QMainWindow*)mariaUI);
 			mariaStateManager->queueState(MariaStateManager::LOADING,temp);
 			temp->setDisplayText("Saving");
 			temp->setLoadingDone();
 			temp->setQuitAfterLoadingTrue();
 			mariaStateManager->transitState();
-		} else if(commandType == MariaInterpreter::CommandType::AddFloatingTask){
-			std::string taskTitle = mariaInterpreter->getTitle(inputText);
+		} else if(input->getCommandType() == MariaInputObject::CommandAdd){
+			MariaTask *toAdd=mariaTaskManager->addTask(input->getTitle(), input->getStartTime(), input->getEndTime());
 
-			MariaTask *toAdd=mariaTaskManager->addTask(taskTitle, NULL, NULL);
 			if(toAdd!=NULL){
-				mariaUI->getCommandBar()->getTextbox()->setQuestionText("Task '"+ taskTitle +"' has been added!");
+				mariaUI->getCommandBar()->getTextbox()->setQuestionText("Task '"+ input->getTitle() +"' has been added!");
 				
 				//Check if the current state is the home state, do a live add.
 				if(mariaStateManager->getCurrentState()==MariaStateManager::STATE_TYPE::HOME) {
@@ -69,44 +69,21 @@ bool MariaLogic::processCommand(std::string inputText) {
 			} else {
 				mariaUI->getCommandBar()->getTextbox()->setQuestionText("There is problem adding '"+ inputText + "'");
 			}
-		} else if (commandType == MariaInterpreter::CommandType::AddDeadlineTask) {
-			std::string taskTitle = mariaInterpreter->getTitle(inputText);
-			MariaTime* endTime = mariaInterpreter->getEndTime(inputText);
-
-			MariaTask *toAdd=mariaTaskManager->addTask(taskTitle, NULL, endTime);
-			if(toAdd!=NULL){
-				mariaUI->getCommandBar()->getTextbox()->setQuestionText("Task '"+ taskTitle +"' has been added!");
-				
-				//Check if the current state is the home state, do a live add.
-				if(mariaStateManager->getCurrentState()==MariaStateManager::STATE_TYPE::HOME) {
-					((MariaUIStateHome*)mariaStateManager->getCurrentStateObject())->addTask(toAdd);
-					mariaFileManager->writeFile(mariaTaskManager->findTask(""));
-				}
-			} else {
-				mariaUI->getCommandBar()->getTextbox()->setQuestionText("There is problem adding '"+ inputText + "'");
-			}
-		} else if (commandType == MariaInterpreter::CommandType::AddTimedTask) {
-			std::string taskTitle = mariaInterpreter->getTitle(inputText);
-			MariaTime* startTime = mariaInterpreter->getStartTime(inputText);
-			MariaTime* endTime = mariaInterpreter->getEndTime(inputText);
-
-			MariaTask *toAdd=mariaTaskManager->addTask(taskTitle, startTime, endTime);
-			if(toAdd!=NULL){
-				mariaUI->getCommandBar()->getTextbox()->setQuestionText("Task '"+ taskTitle +"' has been added!");
-				
-				//Check if the current state is the home state, do a live add.
-				if(mariaStateManager->getCurrentState()==MariaStateManager::STATE_TYPE::HOME) {
-					((MariaUIStateHome*)mariaStateManager->getCurrentStateObject())->addTask(toAdd);
-					mariaFileManager->writeFile(mariaTaskManager->findTask(""));
-				}
-			} else {
-				mariaUI->getCommandBar()->getTextbox()->setQuestionText("There is problem adding '"+ inputText + "'");
-			}
-		} else if (commandType == MariaInterpreter::CommandType::EditTask) {
-			vector<MariaTask*> listOfTasks = mariaTaskManager->findTask(mariaInterpreter->getTitle(inputText));
+		} else if (input->getCommandType() == MariaInputObject::CommandEdit) {
+			vector<MariaTask*> listOfTasks = mariaTaskManager->findTask(input->getTitle());
 
 			if (listOfTasks.size() == 1) {
-				listOfTasks[0]->setTitle(mariaInterpreter->getNewTitle(inputText));
+				switch (input->getEditType()) {
+				case MariaInputObject::EditTitle:
+					listOfTasks[0]->setTitle(input->getEditField());
+					break;
+				case MariaInputObject::EditStartTime:
+					break;
+				case MariaInputObject::EditEndTime:
+					break;
+				default:
+					break;
+				}
 				if (mariaStateManager->getCurrentState() == MariaStateManager::STATE_TYPE::HOME) {
 					((MariaUIStateHome*)mariaStateManager->getCurrentStateObject())->updateTask();
 				}
@@ -115,12 +92,12 @@ bool MariaLogic::processCommand(std::string inputText) {
 			} else {
 				// Conflict! More than 1 task found.
 			}
-		} else if(commandType == MariaInterpreter::CommandType::ShowAllTask){
+		} else if(input->getCommandType() == MariaInputObject::CommandType::CommandShowAll){
 			mariaUI->getCommandBar()->getTextbox()->setQuestionText("Sure, here's a calendar for demo purposes.");
 			mariaStateManager->queueState(MariaStateManager::SHOW,new MariaUIStateShow((QMainWindow*)mariaUI));
 			mariaStateManager->transitState();
-		} else if (commandType == MariaInterpreter::CommandType::DeleteTask) {
-			vector<MariaTask*> listOfTasks = mariaTaskManager->findTask(mariaInterpreter->getTitle(inputText));
+		} else if (input->getCommandType() == MariaInputObject::CommandType::CommandDelete) {
+			vector<MariaTask*> listOfTasks = mariaTaskManager->findTask(input->getTitle());
 
 			if (listOfTasks.size() == 1) {
 				//Check if the current state is the home state, do a live add.
