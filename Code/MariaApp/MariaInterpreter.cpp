9ad50d07b0559561b60b37915b2b9721ce0dd6fb
melvyn <melvyn@localhost>
2014-04-02 13:31:17 +0800

    BugFix: Tasks can now be added whilst in Show State

--- Code/MariaApp/MariaInterpreter.cpp
+++ Code/MariaApp/MariaInterpreter.cpp
@@ -55,7 +55,7 @@ MariaInputObject* MariaInterpreter::parseInput(string inputString, MariaStateMan
 	vector<MariaTime*> timeList;
 	string title = "";
 
-	if (currentState == MariaStateManager::STATE_TYPE::HOME) {
+	if (currentState == MariaStateManager::STATE_TYPE::HOME || currentState == MariaStateManager::STATE_TYPE::SHOW) {
 		// Check for command keyword.
 		commandKeyword = commandKeywordList->find(lowercaseString(tokenizedInput[0]));
 
