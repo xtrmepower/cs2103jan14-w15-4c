56c7e4aaedc6109376eb574e443e082902528269
melvyn <melvyn@localhost>
2014-04-10 17:29:52 +0800

    - Refactored processCommand in MariaLogc.cpp into individual functions. - Add, edit, delete, done, undone can now be performed in SHOW state.

--- Code/MariaApp/MariaLogic.h
+++ Code/MariaApp/MariaLogic.h
@@ -32,5 +32,26 @@ private:
 	MariaTaskManager	*mariaTaskManager;
 	MariaFileManager	*mariaFileManager;
 
+	void runCommandExit(MariaInputObject* input, MariaStateObject* state);
+	void runCommandAddFloatingTask(MariaInputObject* input, MariaStateObject* state);
+	void runCommandAddDeadlineTask(MariaInputObject* input, MariaStateObject* state);
+	void runCommandAddTimedTask(MariaInputObject* input, MariaStateObject* state);
+	void runCommandEditTitle(MariaInputObject* input, MariaStateObject* state);
+	void runCommandEditStartTime(MariaInputObject* input, MariaStateObject* state);
+	void runCommandEditEndTime(MariaInputObject* input, MariaStateObject* state);
+	void runCommandEditDescription(MariaInputObject* input, MariaStateObject* state);
+	void runCommandShowDate(MariaInputObject* input, MariaStateObject* state);
+	void runCommandShowDateRange(MariaInputObject* input, MariaStateObject* state);
+	void runCommandShowAll(MariaInputObject* input, MariaStateObject* state);
+	void runCommandSearch(MariaInputObject* input, MariaStateObject* state);
+	void runCommandDeleteTask(MariaInputObject* input, MariaStateObject* state);
+	void runCommandDeleteAll(MariaInputObject* input, MariaStateObject* state);
+	void runCommandMarkDone(MariaInputObject* input, MariaStateObject* state);
+	void runCommandMarkUndone(MariaInputObject* input, MariaStateObject* state);
+	void runCommandUndo(MariaInputObject* input, MariaStateObject* state);
+	void runCommandGoHome(MariaInputObject* input, MariaStateObject* state);
+	void runCommandPageUp(MariaInputObject* input, MariaStateObject* state);
+	void runCommandPageDown(MariaInputObject* input, MariaStateObject* state);
+
 	static void __cdecl doShowHideWrapper(void* mariaLogic);
 };
